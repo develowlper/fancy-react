d3ad3787feb54cd7042bb152eabeac9444323d65
develowlper <dev@develowlper.de>
2019-02-23 17:35:52 +0100

    added docz build file

---
+++ doczrc.js
@@ -0,0 +1,13 @@
+export default {
+  title: '@fancy-react',
+  description: 'Docs for the fancy-react project',
+  menu: [
+    'Home',
+    'Getting Started',
+    '@fancy-react/ui',
+    '@fancy-react/shop',
+    '@fancy-react/data',
+    '@fancy-react/utils',
+    { name: 'Github', href: 'https://github.com/develowlper/fancy-react' }
+  ]
+};
