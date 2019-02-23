924d96191ec51520078511c59094d344d7c28cf9
develowlper <dev@develowlper.de>
2019-02-23 17:47:55 +0100

    added webpack, docz still running, cosmos still not

--- .docz/app/imports.js
+++ .docz/app/imports.js
@@ -5,12 +5,12 @@ export const imports = {
     import(/* webpackPrefetch: true, webpackChunkName: "docs-home" */ 'docs/Home.mdx'),
   'docs/packages/data/Home.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "docs-packages-data-home" */ 'docs/packages/data/Home.mdx'),
+  'docs/packages/shop/Home.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "docs-packages-shop-home" */ 'docs/packages/shop/Home.mdx'),
   'docs/packages/ui/Home.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "docs-packages-ui-home" */ 'docs/packages/ui/Home.mdx'),
   'docs/packages/utils/Home.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "docs-packages-utils-home" */ 'docs/packages/utils/Home.mdx'),
-  'docs/packages/shop/Home.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "docs-packages-shop-home" */ 'docs/packages/shop/Home.mdx'),
   'packages/utils/src/docs/Home.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "packages-utils-src-docs-home" */ 'packages/utils/src/docs/Home.mdx'),
 }
